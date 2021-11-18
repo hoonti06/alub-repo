b16dd79668a1a70dc63fb2cf3e2a51d49ab7c4bb
hoonti06 <hoonti06@gmail.com>
2021-11-18 22:52:18 +0900

    BOJ-1000 'A+B' [ 21/11/18 22:52:18 ] by Alub

---
+++ BOJ/1000/스터디 채널 테스트.cpp
@@ -0,0 +1,14 @@
+/**
+* 메모리: 1116 KB, 시간: 0 ms
+* 풀이 시간: 00:00:16
+* 2021.11.18
+* by Alub
+*/
+#include <cstdio>
+
+int main()
+{
+    int A, B;
+    scanf("%d%d", &A, &B);
+    printf("%d", A+B);
+}
