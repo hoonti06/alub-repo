20a02008ac20abdb8492285f7a8706c8fd076435
hoonti06 <hoonti06@gmail.com>
2021-11-18 16:42:45 +0900

    BOJ-1085 '직사각형에서 탈출' [ 21/11/18 16:42:45 ] by Alub

---
+++ BOJ/1085/1085_1.java
@@ -0,0 +1,24 @@
+/**
+* 메모리: 18412 KB, 시간: 228 ms
+* 풀이 시간: 00:00:10
+* 2021.11.18
+* by Alub
+*/
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int res = Integer.MAX_VALUE;
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int minWidth = Math.min(x, w-x);
+        int minHeight = Math.min(y, h-y);
+        res = Math.min(minWidth, minHeight);
+        System.out.println(res);
+    }
+}
