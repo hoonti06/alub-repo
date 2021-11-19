1b8f22db213f557fd573fdd336245c702ded6081
hoonti06 <hoonti06@gmail.com>
2021-11-19 12:20:01 +0900

    BOJ-1001 'A-B' [ 21/11/19 12:20:01 ] by Alub

---
+++ BOJ/1001/1001_1.cpp
@@ -0,0 +1,14 @@
+/**
+* 메모리: 1112 KB, 시간: 0 ms
+* 풀이 시간: 00:00:16
+* 2021.11.19
+* by Alub
+*/
+#include <cstdio>
+
+int main()
+{
+    int A, B;
+    scanf("%d%d", &A, &B);
+    printf("%d", A-B);
+}
